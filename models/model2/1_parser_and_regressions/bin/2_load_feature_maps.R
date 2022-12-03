44d56b53689499fc1c106e7cdad33c8854b74bab
Miguel M Álvarez <miguelmartin.alvarez@irbbarcelona.org>
2022-12-03 13:00:39 +0100

    prepared model2

---
+++ models/model2/1_parser_and_regressions/bin/2_load_feature_maps.R
@@ -0,0 +1,211 @@
+library(tidyverse)
+library(dtplyr)
+library(GenomicRanges)
+library(rtracklayer)
+library(valr) # for granges merging
+library("BSgenome.Hsapiens.UCSC.hg19") # all coordinates are in hg19
+library(rlang)
+library(spgs)
+library(conflicted)
+conflict_prefer("filter", "dplyr")
+conflict_prefer("rename", "dplyr")
+conflict_prefer("select", "dplyr")
+conflict_prefer("slice", "dplyr")
+conflict_prefer("map", "purrr")
+conflict_prefer("extract", "magrittr")
+conflict_prefer("reduce", "IRanges")
+
+
+### load chromatin feature maps and dna repair marks hg19 and keep as granges
+
+# from command parameters
+args = commandArgs(trailingOnly=TRUE)
+
+dnarep_marks = ifelse(interactive(),
+                      yes = "../input_lists/dnarep_marks.csv",
+                      no = args[1]) %>%
+  read_csv(comment = "#") 
+
+chromatin_features = ifelse(interactive(),
+                            yes = "../input_lists/chromatin_features.csv",
+                            no = args[2]) %>%
+  read_csv(comment = "#")
+
+chromosome = ifelse(interactive(),
+                    yes = "chr21",
+                    no = paste0("chr", args[3]))
+
+# load collected median_scores from 1st process
+median_scores = lapply(args[-(1:3)], read_tsv) %>%
+  Reduce(function(x, y) bind_rows(x, y), .)
+
+
+dnarep_marks_list_files = list()
+for (feature in dnarep_marks$name){
+
+  path_file = filter(dnarep_marks, name == feature)$path
+
+  dnarep_marks_list_files[[feature]] = tryCatch(import.bw(path_file),
+                                                error = function(e) tryCatch(import.bedGraph(path_file),
+                                                                             error = function(e) tryCatch(makeGRangesFromDataFrame(read_tsv(path_file),
+                                                                                                                                   keep.extra.columns = T),
+                                                                                                          error = function(e) import.bed(path_file))))
+  # extract chromosome (args[3]), otherwise it gets too long and the bed_intersect crashes
+  dnarep_marks_list_files[[feature]] = dnarep_marks_list_files[[feature]][seqnames(dnarep_marks_list_files[[feature]]) == chromosome]
+
+  # add feature name as the metadata (score) colname
+  colnames(elementMetadata(dnarep_marks_list_files[[feature]])) = feature
+  gc()
+}
+
+chromatin_features_list_files = list()
+for (feature in chromatin_features$name){
+
+  path_file = filter(chromatin_features, name == feature)$path
+
+  chromatin_features_list_files[[feature]] = tryCatch(import.bw(path_file),
+                                                      error = function(e) tryCatch(import.bedGraph(path_file),
+                                                                                   error = function(e) tryCatch(makeGRangesFromDataFrame(read_tsv(path_file),
+                                                                                                                                         keep.extra.columns = T),
+                                                                                                                error = function(e) import.bed(path_file))))
+  chromatin_features_list_files[[feature]] = chromatin_features_list_files[[feature]][seqnames(chromatin_features_list_files[[feature]]) == chromosome]
+  gc()
+}
+
+
+## merge Reptime and dna repair coordinates
+
+dfleft = data.frame(chromatin_features_list_files[[1]]) %>% rename("chrom" = "seqnames")
+
+list_feature_names = list()
+list_feature_names[[1]] = colnames(elementMetadata(chromatin_features_list_files[[1]]))
+n_chromatin_features = length(unlist(list_feature_names))
+
+for (feature_i in seq(1, length(dnarep_marks_list_files))){
+
+  dfright = data.frame(dnarep_marks_list_files[[feature_i]]) %>% 
+    rename("chrom" = "seqnames")
+  
+  list_feature_names[[n_chromatin_features + feature_i]] = colnames(elementMetadata(dnarep_marks_list_files[[feature_i]]))
+
+  merged_temp = bed_intersect(dfleft, dfright, suffix = c("_dfleft",
+                                                          "_dfright")) %>%
+    mutate(start = ifelse(start_dfleft >= start_dfright,
+                          start_dfleft,
+                          start_dfright),
+           end = ifelse(end_dfleft <= end_dfright,
+                        end_dfleft,
+                        end_dfright),
+           strand = "*",
+           width = end - start + 1) %>%
+    select(chrom, start, end, width, strand, contains(unlist(list_feature_names)))
+  colnames(merged_temp) = gsub("_dfleft", "", colnames(merged_temp))
+  colnames(merged_temp) = gsub("_dfright", "", colnames(merged_temp))
+
+  dfleft = merged_temp
+  rm(merged_temp) ; rm(dfright)
+  gc()
+}
+
+rm(dnarep_marks_list_files) ; gc()
+
+map_features = dfleft %>%
+  rename("seqnames" = "chrom") %>%
+  arrange(start)
+
+rm(dfleft) ; gc()
+
+# this will go to last process
+write_tsv(map_features, paste0("map_features_", chromosome, ".tsv"))
+# map_features = read_tsv("map_features_chr21.tsv")
+
+
+#### binarize scores, and get frequency of each trinucleotide per sequence (for 3rd process -- offset)
+
+## binarize weighted average DNA repair value by being lower or larger than the across-genome median
+map_features_binarized_temp = map_features %>%
+  lazy_dt %>% 
+  #### WARNING first do the average score at duplicated (start end) ranges, this is due to the hg38-->hg19 lift dividing some ranges into 2 alternative ranges with the same score
+  group_by(seqnames, start, end) %>% 
+  summarise_at(vars(!matches("seqnames|start|end|width|strand")),
+               ~mean(.)) %>% 
+  # there are some duplicated only at the start
+  group_by(seqnames, start) %>% 
+  summarise_at(vars(!matches("seqnames|start|width|strand")),
+               ~mean(.)) %>% 
+  # there are some duplicated only at the start
+  group_by(seqnames, end) %>% 
+  summarise_at(vars(!matches("seqnames|end|width|strand")),
+               ~mean(.)) %>% 
+  ungroup %>% 
+  as_tibble %>% 
+  rowwise %>% 
+  lazy_dt %>% 
+  mutate_at(vars(dnarep_marks$name),
+            function(x){var_name = rlang::as_label(substitute(x))
+            ifelse(x <= filter(median_scores, dnarep_mark == var_name) %>% pull(median_score),
+                   "low",
+                   "high")}) %>% 
+  as_tibble %>% 
+  unite("metadata", !matches("seqnames|start|end|width|strand")) %>% 
+  makeGRangesFromDataFrame(keep.extra.columns = T)
+## collapse contiguous ranges if they have same metadata levels
+map_features_binarized_temp = unlist(reduce(split(map_features_binarized_temp, ~metadata)))
+mcols(map_features_binarized_temp) = names(map_features_binarized_temp)
+map_features_binarized = map_features_binarized_temp %>% 
+  as_tibble %>% 
+  arrange(start) %>% 
+  separate(X, into = c(chromatin_features$name, dnarep_marks$name), sep = "_") %>% 
+  # add 1 bp downstream and upstream regions of width 1 or 2, so trinucs can be fetched
+  mutate(start = ifelse(width <= 2,
+                        start-1,
+                        start),
+         end = ifelse(width <= 2,
+                      end+1,
+                      end),
+         width = end-start+1)
+
+gc()
+
+
+# get sequence for each range
+sequences = getSeq(BSgenome.Hsapiens.UCSC.hg19,
+                   names = makeGRangesFromDataFrame(map_features_binarized, 
+                                                    keep.extra.columns = T))
+
+# get frequency of each trinucleotide per sequence, moving 1 nucleotide downstream each time
+trinuc32_freq = trinucleotideFrequency(sequences) %>%
+  as_tibble %>%
+  rownames_to_column("id") %>% 
+  lazy_dt %>%
+  pivot_longer(cols = -id,
+               names_to = 'trinuc32',
+               values_to = "freq") %>%
+  group_by(id) %>%
+  # trinucs that do not have a C or T in center, convert to reverse complement
+  mutate(trinuc32 = ifelse(substr(trinuc32, start = 2, stop = 2) %in% c('A', 'G'),
+                           spgs::reverseComplement(trinuc32, case="upper"),
+                           trinuc32)) %>% 
+  # sum up frequencies of each N(C|T)N & reverse complement pair, within id
+  group_by(id, trinuc32) %>%
+  summarise(freq = sum(freq)) %>%
+  ungroup %>% 
+  # back to original format (each row ('id') maps to the same row in map_features)
+  pivot_wider(names_from = 'trinuc32',
+              values_from = 'freq') %>% 
+  as_tibble %>% 
+  arrange(as.numeric(id)) %>% 
+  select(-id)
+gc()
+
+# bind trinuc32 freqs to map_features_binarized
+map_features_binarized_trinuc32_freq = map_features_binarized %>% 
+  bind_cols(trinuc32_freq) %>% 
+  group_by_at(vars(c(chromatin_features$name, dnarep_marks$name))) %>% 
+  summarise_at(vars(matches("^[A,C,T,G][C,T][A,C,T,G]$")),
+               ~sum(.)) %>% 
+  mutate(chrom = chromosome) %>% 
+  relocate(chrom)
+gc()
+
+write_tsv(map_features_binarized_trinuc32_freq, paste0("map_features_binarized_", chromosome, ".tsv"))
---
+++ models/model2/1_parser_and_regressions/run_nf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export TOWER_ACCESS_TOKEN="eyJ0aWQiOiA0ODk1fS4yNDdkNzEyYmY5NGUxMmFlOTQ1OGYwYWJlYmI2MjY0YmU2Y2E4Yzdl"
+
+mkdir -p log/
+
+nextflow -log $PWD/log/nextflow.log run pipe.nf --dnarep_marks $PWD/input_lists/dnarep_marks.csv \
+												--chromatin_features $PWD/input_lists/chromatin_features.csv \
+												--sample_ids $PWD/input_lists/sample_ids.csv \
+												--somatic_data /g/strcombio/fsupek_cancer3/malvarez/WGS_tumors/somatic_variation/cell_lines/kucab_2019/processed/data/muts_pass_ \
+												--metadata /g/strcombio/fsupek_cancer3/malvarez/WGS_tumors/somatic_variation/cell_lines/kucab_2019/processed/sample_treatments.tsv \
+												--memory_process1 6 \
+												--memory_process2 40 \
+												--memory_process3 2 \
+												--memory_process4 35 \
+												-resume \
+												-with-tower
